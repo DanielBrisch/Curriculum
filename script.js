195bb6fff3b5d7f5ac4fccfcabb84d68169cb22e
DaneilBrisch <danielbrisch157@gmail.com>
2023-09-04 21:22:18 -0300

    06

--- script.js
+++ script.js
@@ -1,19 +1,55 @@
-divExperiencias = document.querySelector("#experience")
+divExperiencias = document.querySelector("#experiences")
 
+console.log(divExperiencias)
 experiencias = [
     {
-        "empresa": "Empresa X",
-        "cargo": "Cargo",
+        "empresa": "Unimed",
+        "cargo": "Programador Full Stack Java",
         "descricao": "descricao"
-    }
+    },
+    {
+        "empresa": "Viasoft",
+        "cargo": "Programador Full Stack Java",
+        "descricao": "descricao"
+    },
+    {
+        "empresa": "Viasoft",
+        "cargo": "Programador Full Stack Java",
+        "descricao": "descricao"
+    },
 ]
 
-divExperience = document.createElement("div")
-document.setAttribute("class", "experience")
 
-empresa = document.createElement("h3")
-empresa.setAttribute("class", "empresa")
-empresa.innetText = experiencias[0].empresa
 
-divExperience.appendChild(empresa)
-divExperiencias.appendChild(divExperience)
+function createDivExperiencia(divPai, experiencia){
+    divExperience = document.createElement("div")
+    divExperience.setAttribute("class", "experience")
+
+    empresa = document.createElement("h3")
+    empresa.setAttribute("class", "empresa")
+    empresa.innerText = experiencia.empresa
+
+    cargo = document.createElement("h4")
+    cargo.setAttribute("class", "cargo")
+    cargo.innerText = experiencia.cargo
+
+    descricao = document.createElement("p")
+    descricao.setAttribute("class", "descricao")
+    descricao.innerText = experiencia.descricao
+
+    divExperience.appendChild(empresa)
+    divExperiencias.appendChild(divExperience)
+
+    divExperience.appendChild(cargo)
+    divExperiencias.appendChild(divExperience)
+
+    divExperience.appendChild(descricao)
+    divPai.appendChild(divExperience)
+}
+
+
+
+
+for (let experiencia of experiencias){
+    createDivExperiencia(divExperiencias, experiencia)
+}
