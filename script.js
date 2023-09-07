d8c6b26df1d71299bbe3b94ec91c5e4b83f8bc3f
DanielBrisch <danielbrisch157@gmail.com>
2023-09-07 01:25:41 -0300

    09

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ experiencias = [
         "empresa": "Unimed",
         "cargo": "Jovem Aprendiz de Analista de Infraestrutura",
         "descricao": "Por um período próximo a um ano, exerci a posição de suporte de infraestrutura, com responsabilidades voltadas para a manutenção de máquinas e redes na Unimed Pato Branco.",
-        "data": "01/03/2022 - 30/12/2022"
     },
     {
         "empresa": "Viasoft",
         "cargo": "Analista De Testes",
         "descricao": "Atuei como analista de testes na Viasoft, na área contábil do sistema de agronegócio, por um período de 7 meses. Durante esse tempo, desempenhei frequentemente o papel de analista de requisitos e trabalhei extensivamente na elaboração e execução de consultas e queries necessárias para o desenvolvimento de código.",
-        "data": "02/01/2023 - 01/07/2023"
     },
     {
         "empresa": "Viasoft",
         "cargo": " ",
         "descricao": "Dentro da Viasoft, realizei uma transição de função para o cargo de desenvolvedor full stack, onde trabalhei com um amplo leque de tecnologias, incluindo Java, Spring Boot, Angular, TypeScript, Android Native e Flutter.",
-        "data": "01/07/2023 - Atualmente"
+
     },
 ]
 
@@ -49,9 +47,6 @@ function createDivExperiencia(divPai, experiencia){
     divExperiencias.appendChild(divExperience)
 
     divExperience.appendChild(descricao)
-    divPai.appendChild(divExperience)
-
-    divExperience.appendChild(data)
     divPai.appendChild(divExperience)
 
 }
